21b6583e1c64734e2d358d85acc08d8791b69343
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-10-22 19:43:56 +0530

    tree

---
+++ Binary_tree.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<malloc.h>
+struct node {
+    int data;
+    struct node* left;
+    struct node * right;
+};
+struct node *createnode(int data)
+{
+    struct node *new;
+    new= (struct node*)malloc(sizeof(struct node));
+    new->data=data;
+    new->left=NULL;
+    new->right=NULL;
+}
+int main()
+{
+    struct node *s= createnode(18);
+    struct node *s1= createnode(7);
+    struct node *s2= createnode(45);
+    
+    // struct node *s1= createnode(33);
+   s->left=s1;
+   s->right=s2;
+
+}
