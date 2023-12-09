70cb5394298f3cbffee97b804af27ae8218e1a7b
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-12-09 21:09:08 +0530

    Delete at Last

---
+++ DD-LL3.C
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+// trasversal in both direction in a doubly linked list.
+struct node
+{
+    int data;
+    struct node *pre;
+    struct node *next;
+};
+
+
+struct node *delatlast(struct node *head)
+{
+
+    struct node *ptr = head;
+    struct node *p = head->next;
+
+    while (p->next != NULL)
+    {
+        p = p->next;
+        ptr = ptr->next;
+    }
+    ptr->next = NULL;
+
+    free(p);
+
+    return head;
+}
+int *trasversalf(struct node *ptr)
+{
+    printf("Trasvering in forward didection\n");
+    do
+    {
+        printf("Element :%d\n", ptr->data);
+        ptr = ptr->next;
+    } while (ptr != NULL);
+}
+int *trasversalb(struct node *ptr)
+{
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    printf("Trasvering in backward didection\n");
+    do
+    {
+        printf("Element :%d\n", ptr->data);
+        ptr = ptr->pre;
+    } while (ptr != NULL);
+}
+int main()
+{
+    struct node *head;
+    struct node *first;
+    struct node *second;
+    struct node *third;
+    head = (struct node *)malloc(sizeof(struct node));
+    first = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 1;
+    head->next = first;
+    head->pre = NULL;
+
+    first->data = 2;
+    first->next = second;
+    first->pre = head;
+
+    second->data = 3;
+    second->next = third;
+    second->pre = first;
+
+    third->data = 4;
+    third->next = NULL;
+    third->pre = second;
+    head = delatlast(head);
+
+    trasversalf(head);
+    trasversalb(head);
+
+    return 0;
+}
