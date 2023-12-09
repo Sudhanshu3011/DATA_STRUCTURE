063e9a73163a81e5470a2a6e73741a856a497955
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-12-09 21:06:44 +0530

    Insert at First

---
+++ DD-LL1.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+// trasversal in both direction in a doubly linked list.
+struct node
+{
+    int data;
+    struct node *pre;
+    struct node *next;
+};
+
+struct node *createnew(int data)
+{
+    struct node *new;
+    new = (struct node *)malloc(sizeof(struct node));
+    new->data = data;
+    new->next = new->pre = NULL;
+
+    return new;
+}
+struct node *insertatfirst(struct node *head, int data)
+{
+    struct node *newhead = createnew(data);
+    newhead->pre = NULL;
+    newhead->next = head;
+    head->pre = newhead;
+
+    head = newhead;
+    return head;
+}
+int *trasversalf(struct node *ptr)
+{
+    printf("Trasvering in forward didection\n");
+    do
+    {
+        printf("Element :%d\n", ptr->data);
+        ptr = ptr->next;
+    } while (ptr != NULL);
+}
+int *trasversalb(struct node *ptr)
+{
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    printf("Trasvering in backward didection\n");
+    do
+    {
+        printf("Element :%d\n", ptr->data);
+        ptr = ptr->pre;
+    } while (ptr != NULL);
+}
+int main()
+{
+    struct node *head;
+    struct node *first;
+    struct node *second;
+    struct node *third;
+    head = (struct node *)malloc(sizeof(struct node));
+    first = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 1;
+    head->next = first;
+    head->pre = NULL;
+
+    first->data = 2;
+    first->next = second;
+    first->pre = head;
+
+    second->data = 3;
+    second->next = third;
+    second->pre = first;
+
+    third->data = 4;
+    third->next = NULL;
+    third->pre = second;
+    head = insertatfirst(head, 0);
+
+    trasversalf(head);
+    trasversalb(head);
+
+    return 0;
+}
